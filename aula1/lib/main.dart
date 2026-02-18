11aa63e4b3ce42441f0e6dbbb79f6f4efa891899
Julia Alves Pereira <Juliaalvespereira01@gmail.com>
2026-02-18 11:14:40 -0300

    Criando Aplicativo

--- aula1/lib/main.dart
+++ aula1/lib/main.dart
@@ -9,6 +9,38 @@ class TelaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold( //Fragmenta sua tela
+        appBar: AppBar(title: Text("App1")),
+        body:Column(
+          spacing: 20,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(width: 100, height: 100, color: Colors.pinkAccent),
+            Container(width: 100, height: 100, color: Colors.black),
+            Container(width: 100, height: 100, color: Colors.amber),
+            Row(
+              spacing: 20,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 100, height: 100, color: const Color.fromARGB(255, 64, 255, 89)),
+                Container(width: 100, height: 100, color: const Color.fromARGB(255, 192, 55, 158)),
+                Container(width: 100, height: 100, color: const Color.fromARGB(255, 67, 22, 138)),
+              ],
+            ),
+            SizedBox(height: 50),
+            Stack(
+              alignment: AlignmentGeometry.center,
+              children: [
+                Container(width: 100, height: 100, color: const Color.fromARGB(255, 70, 155, 224)),
+                Container(width: 70, height: 70, color: const Color.fromARGB(255, 87, 84, 143)),
+                Container(width: 50, height: 50, color: const Color.fromARGB(255, 86, 148, 96)),
+              ],
+            )
+          ],
+        ),
+      )
+    );
   }
 }
