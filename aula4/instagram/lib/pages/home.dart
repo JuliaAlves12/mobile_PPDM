e3f6e4ff3b29f234a4c1a243fb90ab201ffa3071
Julia Alves Pereira <Juliaalvespereira01@gmail.com>
2026-04-08 10:41:52 -0300

    Flutter - dart project

--- aula4/instagram/lib/pages/home.dart
+++ aula4/instagram/lib/pages/home.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
+List nomes = [
+  'seu story',
+  'Rayane',
+  'Gabi',
+  'Nicolas',
+  'Dienifer',
+  'Julia',
+  'Joao',
+  'Lucas',
+  'Mariam',
+  'Kaique',
+];
+List images = [
+  'https://images.unsplash.com/photo-1775563622936-2bac8f284416?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1774860390220-5f568d607636?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1774991061995-9bef4c333de4?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://plus.unsplash.com/premium_photo-1674777843203-da3ebb9fbca0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1565958011703-44f9829ba187?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://plus.unsplash.com/premium_photo-1664478383014-e8bc930be7c2?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1556742502-ec7c0e9f34b1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1627647227768-705244233b56?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://plus.unsplash.com/premium_photo-1675432656807-216d786dd468?q=80&w=690&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+];
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -11,23 +36,40 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.white,
         title: Text(
           'Instagram',
-          style: TextStyle(color: Colors.black, fontSize: 28, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         actions: [
-          IconButton(icon: Icon(Icons.add_box_outlined, color: Colors.black), onPressed: () {}),
-          IconButton(icon: Icon(Icons.favorite_border, color: Colors.black), onPressed: () {}),
-          IconButton(icon: Icon(Icons.chat_bubble_outline, color: Colors.black), onPressed: () {}),
+          IconButton(
+            icon: Icon(Icons.add_box_outlined, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite_border, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.chat_bubble_outline, color: Colors.black),
+            onPressed: () {},
+          ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
+            SizedBox(
+              width: double.infinity,
               height: 110,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                itemCount: 10,
+                physics: AlwaysScrollableScrollPhysics(),
+                itemCount: nomes.length,
                 itemBuilder: (context, index) {
+                  final itemnomes = nomes[index];
+                  final itemimagens = images[index];
                   return Column(
                     children: [
                       Container(
@@ -36,25 +78,108 @@ class Home extends StatelessWidget {
                         height: 70,
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
-                          gradient: LinearGradient(colors: [Colors.yellow, Colors.red, Colors.purple]),
+                          gradient: LinearGradient(
+                            colors: [Colors.yellow, Colors.red, Colors.purple],
+                          ),
                         ),
                         child: Padding(
                           padding: EdgeInsets.all(3),
                           child: Container(
-                            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-                            child: CircleAvatar(backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0YjkcW6qZ-cevjZgMz5vEgrN2_fYZ8W64Lw&s")),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(itemimagens),
+                            ),
                           ),
                         ),
                       ),
-                      Text("Nome", style: TextStyle(fontSize: 12)),
+                      Text(itemnomes, style: TextStyle(fontSize: 12)),
                     ],
                   );
-                }
+                },
               ),
-            )
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(images[2]),
+                  ),
+                  title: Text(
+                    nomes[2],
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  trailing: Icon(Icons.more_vert),
+                ),
+                Padding(
+                  padding: EdgeInsetsGeometry.all(5),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(
+                      'assets/pecadores.jpg',
+                      width: double.infinity,
+                      height: 600,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.favorite_border),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.chat_bubble_outline),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.send_outlined),
+                      onPressed: () {},
+                    ),
+                    Spacer(),
+                    IconButton(
+                      icon: Icon(Icons.bookmark_border),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(images[3]),
+                            radius: 11,
+                          ),
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(images[7]),
+                            radius: 11,
+                          ),
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(images[1]),
+                            radius: 11,
+                          ),
+                          Text(
+                            'Curtido por Nicolas e outras pessoas',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
